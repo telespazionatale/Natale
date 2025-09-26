e72ec30bcb5d8999e20757c2f6a9d267d1f0079a
Natale <gabriele.natale@telespazio.com>
2025-09-26 04:54:43 -0400

    ciao

---
+++ calculateArea/calculate_Area.js
@@ -0,0 +1,7 @@
+let length;
+let width;
+function calculateArea() {
+ length = parseFloat(document.getElementById('length').value);
+ width = parseFloat(document.getElementById('width').value);
+ let area = length * width; 
+}
