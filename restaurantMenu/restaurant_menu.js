168dc23b11d26477844b1637b65cea758995c765
Telespazionatale <gabriele.natale@telespazio.com>
2025-10-07 06:38:14 -0400

    message

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict- $22.99', 'Oatmeal- $21.99', 'Frittata- $15'];
+const mainCourseMenu = ['Steak- $25', 'Pasta- $15', 'Burger- 22$', 'Salmon- $20'];
+const dessertMenu = ['Cake- $10', 'Ice Cream- $5', 'Pudding- $8', 'Fruit Salad- $10'];
  const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
         document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
          let mainCourseItem = '';
